9f273f5a2caff678fec08ad69da76228bf3accbf
WhiteApfel <github@pfel.ru>
2019-05-12 22:35:55 +0700

    Fix Style +

--- button.reader.js
+++ button.reader.js
@@ -4,6 +4,6 @@ else{var buttoncheck = false}
 if(buttoncheck){var readerbutton = document.createElement("div");
 readerbutton.setAttribute("id", "ReaderModeButton8921");
 readerbutton.setAttribute("style","position:absolute;top:4em;right:0;padding:0.5em 0.75em;background-color:#000;opacity:0.7;z-index:999999;color:#F60");
-readerbutton.setAttribute("onclick","var sc = document.createElement('script');sc.src='https://reader.pfel.ru/reader.js';document.body.appendChild(sc);if(document.getElementsByTagName('style')[0]!=null){document.head.removeChild(document.getElementsByTagName('style')[0]);}var countlink = 0;if(document.getElementsByTagName('link')[countlink]!=null){document.head.removeChild(document.getElementsByTagName('link')[countlink]);countlink++;}");
+readerbutton.setAttribute("onclick","var sc = document.createElement('script');sc.src='https://reader.pfel.cc/beta.reader.js';document.body.appendChild(sc);if(document.getElementsByTagName('style')[0]!=null){document.head.removeChild(document.getElementsByTagName('style')[0]);};var countlink = 0;");
 readerbutton.innerHTML = "R";
 document.body.appendChild(readerbutton);}})()
