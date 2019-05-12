90837c0214e1358a3b70b2411d4d9b0f0dccd638
WhiteApfel <github@pfel.ru>
2019-05-12 21:41:55 +0700

    Create button.reader.js

---
+++ button.reader.js
@@ -0,0 +1,9 @@
+(function(){
+if(document.getElementById('ReaderModeButton8921')==null){var buttoncheck = true}
+else{var buttoncheck = false}
+if(buttoncheck){var readerbutton = document.createElement("div");
+readerbutton.setAttribute("id", "ReaderModeButton8921");
+readerbutton.setAttribute("style","position:absolute;top:4em;right:0;padding:0.5em 0.75em;background-color:#000;opacity:0.7;z-index:999999;color:#F60");
+readerbutton.setAttribute("onclick","var sc = document.createElement('script');sc.src='https://reader.pfel.ru/reader.js';document.body.appendChild(sc);if(document.getElementsByTagName('style')[0]!=null){document.head.removeChild(document.getElementsByTagName('style')[0]);}var countlink = 0;if(document.getElementsByTagName('link')[countlink]!=null){document.head.removeChild(document.getElementsByTagName('link')[countlink]);countlink++;}");
+readerbutton.innerHTML = "R";
+document.body.appendChild(readerbutton);}})()
