1842491dfcee892116cf8455f2868866aa99a258
WhiteApfel <github@pfel.ru>
2019-05-13 18:33:56 +0700

    Исправление стилей

--- button.reader.js
+++ button.reader.js
@@ -1,9 +1,17 @@
-(function(){
-if(document.getElementById('ReaderModeButton8921')==null){var buttoncheck = true}
-else{var buttoncheck = false}
-if(buttoncheck){var readerbutton = document.createElement("div");
-readerbutton.setAttribute("id", "ReaderModeButton8921");
-readerbutton.setAttribute("style","position:absolute;top:4em;right:0;padding:0.5em 0.75em;background-color:#000;opacity:0.7;z-index:999999;color:#F60");
-readerbutton.setAttribute("onclick","var sc = document.createElement('script');sc.src='https://reader.pfel.cc/beta.reader.js';document.body.appendChild(sc);if(document.getElementsByTagName('style')[0]!=null){document.head.removeChild(document.getElementsByTagName('style')[0]);};var countlink = 0;");
-readerbutton.innerHTML = "R";
-document.body.appendChild(readerbutton);}})()
+(
+	function()
+	{
+		if(document.getElementById('ReaderModeButton8921')==null)
+			{var buttoncheck = true}
+		else{var buttoncheck = false}
+		if(buttoncheck)
+		{
+			var readerbutton = document.createElement("div");
+			readerbutton.setAttribute("id", "ReaderModeButton8921");
+			readerbutton.setAttribute("style","position:absolute;top:4em;right:0;padding:0.5em 0.75em;background-color:#000;opacity:0.7;z-index:999999;color:#F60");
+			readerbutton.setAttribute("onclick","var titlepage = document.getElementsByTagName('title')[0];document.head.innerHTML = ' ';document.head.appendChild(titlepage);var sc = document.createElement('script');sc.src='https://reader.pfel.ru/reader.js';document.body.appendChild(sc);");
+			readerbutton.innerHTML = "R";
+			document.body.appendChild(readerbutton);
+		}
+	}
+)()
